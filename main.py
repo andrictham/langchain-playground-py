37ccf43cad3ebf4ddca0e307d562031bff1272ab
sprazzeus <sprazzeus@gmail.com>
2023-03-02 15:33:21 +0800

    Use OpenAIChat, remove streaming

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import streamlit as st
 
 from langchain.chains import LLMChain, SimpleSequentialChain
-from langchain.llms import OpenAI
+from langchain.llms import OpenAIChat
 from langchain.prompts import PromptTemplate
 from langchain.callbacks.base import CallbackManager
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
@@ -15,7 +15,7 @@ st.title('LangChain Sequential Chain Playground')
 
 user_question = st.text_input('What is your question?')
 
-llm = OpenAI(streaming=True, callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]), verbose=True, temperature=0.0, openai_api_key=OPENAI_API_KEY)
+llm = OpenAIChat(verbose=True, temperature=0.0, openai_api_key=OPENAI_API_KEY)
 
 if st.button("Tell me about it", type="primary"):
     # Chain 1
